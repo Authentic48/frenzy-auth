b54a13d26dca749c7f3c6d42ffc2e558830b5622
Fall Ibrahim <ibrahim_fall@smartworld.team>
2023-02-26 17:29:03 +0700

    feat(ttk-6): remove twilio

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,15 +7,12 @@ import { PrismaService } from './prisma/prisma.service';
 import { UserModule } from './modules/user/user.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { ScheduleModule } from '@nestjs/schedule';
-// import { TwilioModule } from 'nestjs-twilio';
-// import { getTwilioConfig } from './configs/twilio.config';
 
 @Module({
   imports: [
     RMQModule.forRootAsync(getRMQConfig()),
     ConfigModule.forRoot({ isGlobal: true, validationSchema: configSchema }),
     ScheduleModule.forRoot(),
-    // TwilioModule.forRootAsync(getTwilioConfig()),
     UserModule,
     AuthModule,
   ],
